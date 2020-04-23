c1ec1cbe62a898d3ed1e90e40692d283e0fd4e5c
= <=>
2020-04-23 14:40:07 -0700

    beer

--- comps/ingredients/about/index.js
+++ comps/ingredients/about/index.js
@@ -5,12 +5,14 @@ import BasicButton from '../../buttons/button1';
 const defaultImg = require('./ammonia.png');
 
 const IngredAbout = ({img,text}) => <div className="about">
+
     <div className="about_inner">
     <div className="about_img"> <img src={img} /></div>
     <div className="about_info">{text}</div>
-    <div className="backbutton">
+
     <BasicButton text="Back" bgcolor="rgb(105,138,137)"/>
-    </div>
+  
+
     </div>
 </div>;
 
