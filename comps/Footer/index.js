faab02c1cc68f27853470edb711ff16e3c8bd18e
yinayoon419 <=>
2020-04-23 14:54:05 -0700

    put button in footer

--- comps/Footer/index.js
+++ comps/Footer/index.js
@@ -1,9 +1,12 @@
 
 import React from 'react';
 import './footer.css';
+import BasicButton from '../buttons/button1';
 
 
 const Footer = () => <div className="footer">
+        <div className="button_down"><BasicButton /></div>
+        
 </div>;
 
 Footer.defaultProps = {
