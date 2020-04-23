08fc65ffe3a95099df12e8f335ca146ee3e1f6cf
= <=>
2020-04-23 05:17:12 -0700

    buttons

---
+++ comps/buttons/button2/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './button2.css';
+
+const StartButton = ({text, bgcolor, onMouseOver, onMouseOut}) => <div 
+    style={{backgroundColor:bgcolor}}
+    className="startbutton_box"
+    onMouseOver={onMouseOver}
+    onMouseOut={onMouseOut}
+    >
+    <div className="startbutton_box_inner">
+        {text}
+    </div>
+</div>
+
+StartButton.defaultProps = {
+    text:"Default Button",
+    bgcolor: "#c8553D",
+    onMouseOver:MouseOver,
+    onMouseOut:MouseOut
+}
+
+function MouseOver(){
+    document.querySelector(".startbutton_box").style.backgroundColor = "white";
+    document.querySelector(".startbutton_box").style.border = "2px solid #c8553D";
+    document.querySelector(".startbutton_box").style.color = "#c8553D";
+}
+function MouseOut(){
+    document.querySelector(".startbutton_box").style.backgroundColor = "#c8553D";
+    document.querySelector(".startbutton_box").style.border = "none";
+    document.querySelector(".startbutton_box").style.color = "white";
+}
+
+
+export default StartButton;
--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import BasicButton from '../comps/buttons/button1';
+import StartButton from '../comps/buttons/button2';
 
 
 export default {
@@ -12,4 +13,20 @@ export const MyBasicButton = () => <BasicButton />;
 export const PageWithCustomButtons = () => <div>
     <BasicButton text="Next" />
     <BasicButton text="Back" bgcolor="rgb(105,138,137)"/>
+</div>
+
+
+
+
+export const MyStartButton = () => <StartButton />;
+export const PageWithButtons = () => <div>
+    <div>
+    <StartButton text="Start" bgcolor="#c8553D" />
+    </div>
+    <div>
+    <StartButton text="Basic" bgcolor="#F28F3c" /> 
+    </div>
+    <div>
+    <StartButton text="Advanced" bgcolor="#707070" />
+    </div>
 </div>
