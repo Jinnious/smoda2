c42e5731b553d3cdc73a03e8f8c96f10c82f0d87
yinayoon419 <=>
2020-04-24 02:21:23 -0700

    answer question

---
+++ comps/Quiz/Answers/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './question.css';
+import BasicButton from '../../buttons/button1';
+
+const defaultImg = require('./circle.png');
+const defaultImg2 = require('./wrong.png');
+
+
+const QuestionAnswer = ({img1,img2,text1,text2}) => <div className="about">
+
+    <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet"></link>
+    <div className="question_inner">
+    <div className="about_info">{text1}
+    <div className="quizicon">
+    <div className="right"> <img src={img1} /></div>
+    <div className="wrong"> <img src={img2} /></div>
+
+   </div>
+   </div>
+
+ <p className="quizbuttons">
+     <div className="quizback"><BasicButton text="Back" bgcolor="rgb(105,138,137)"/></div>
+ <div className="quiznext"><BasicButton text="Next" bgcolor="rgb(99,77,55)"/></div></p>
+
+  
+  
+
+    </div>
+</div>;
+
+QuestionAnswer.defaultProps = {
+    img1:defaultImg,
+    img2:defaultImg2,
+    text1: "Is nicotine an ingredient of cigarette?",
+    text2: "Yes. Nicotine is an ingredient of cigarette."
+}
+
+
+export default QuestionAnswer;
---
+++ stories/quizanswer.stories.js
@@ -0,0 +1,43 @@
+import React from 'react';
+// import defaultImg1 from '../comps/Quiz/Questions';
+// import defaultImg2 from '../comps/Quiz/Questions';
+import Questions from '../comps/Quiz/Questions';
+import QuizSubHead1  from './subhead.stories';
+import SubHeader from '../comps/subheader';
+
+
+export default {
+    title: 'Quiz 1',
+    componets: Quiz1
+};
+
+
+export const Quiz1 = () =><div>
+    <div><SubHeader text="Quiz 1" color="#F28F3C"/></div>
+    <div><Questions /></div>
+</div>
+
+export const Quiz2 = () => <div>
+    <div><SubHeader text="Quiz 2" color="#F28F3C"/></div>
+<div><Questions text="If you smoke, does your brain size increase?"/></div>
+</div>
+
+export const Quiz3 = () => <div>
+        <div><SubHeader text="Quiz 3" color="#F28F3C"/></div>
+
+    <div><Questions text="You may have damaged sense of taste if you smoke." /></div>
+    </div>
+
+export const Quiz4 = () => 
+<div>
+        <div><SubHeader text="Quiz 4" color="#F28F3C"/></div>
+
+    <div><Questions text="Smoking may give higher risks of lung cancer." /></div>
+</div>
+export const Quiz5 = () => 
+
+<div>
+        <div><SubHeader text="Quiz 5" color="#F28F3C"/></div>
+
+    <div><Questions text="Smoking is actually good for your heart, as it increases blood circulations." /></div>
+</div>
