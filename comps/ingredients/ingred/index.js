18d024856a7aebef225af7c5cd63fd92aaf1e999
Juhye Kim <jujubebe@Juhyes-iMac.local>
2020-04-24 00:58:23 -0700

    add organ

--- comps/ingredients/ingred/index.js
+++ comps/ingredients/ingred/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ingred.css';
 import SubHeader from '../../subheader';
-import BasicButton from '../../buttons/button1';
+
 import SmokCiga from '../ciga';
 
 
@@ -10,11 +10,6 @@ const SmokIngred = () => <div className="smoking">
     <div><SubHeader text="Ingredients" color="#C8553D"/></div>
     <div className="ciga_title">What is in a cigarette?</div>
     <div className="cigaImg"><SmokCiga /></div>
-    <div className="footer">
-        <BasicButton text="Back" bgcolor="rgb(105,138,137)" />
-        <BasicButton text="Next" bgcolor="rgb(242,143,60)" />
-
-    </div>
 </div>;
 
 
