9a7d1894d7e4fa90f625c834e5c45950fa1028a7
yinayoon419 <=>
2020-04-24 03:57:39 -0700

    description box

--- stories/whitecomponent.stories.js
+++ stories/whitecomponent.stories.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import EffectDescription from '../comps/EffectDescription';
+import teeth from '../comps/EffectDescription/teeth.png';
+import lung from '../comps/EffectDescription/lung.png';
+import heart from '../comps/EffectDescription/heart.png';
+import brain from '../comps/EffectDescription/brain.png';
+
 
 export default {
     title: 'EffectDescription',
@@ -7,14 +12,29 @@ export default {
 };
 
 
-export const D1 = () => <EffectDescription />
+export const LungD = () => <EffectDescription />
 
-export const D2 = () => <EffectDescription text="Benzene causes harmful effects on the bone marrow and can cause a decrease in red blood cells. It can also cause a lot of bleeding and can affect the immune system, increasing the chance for infection." />
+export const LungD2 = () => <EffectDescription text="Smoking puts you at a significantly higher risk of developing lung cancer." />
 
-export const D3 = () => <EffectDescription text="Butane slows down the activity of the brain, affecting physical
-and mental responses. When butane is inhaled, the fumes are absorbed rapidly through the lungs into the
-bloodstream." />
+export const LungD3 = () => <EffectDescription text="" img={lung} />
 
-export const D4 = () => <EffectDescription text="Nicotine is a chemical compound that is present in tobacco. Nicotine is a dangerous and highly addictive chemical. It can cause an increase in blood pressure, heart rate, flow of blood to the heart." />
 
-export const D5 = () => <EffectDescription text=" Tar is the word for the solid particles suspended in tobacco smoke. The particles contain chemicals, including cancer-causing substances. Tar is sticky and brown, and stains teeth, fingernails." />
+export const HeartD= () => <EffectDescription text="Smoking damages your heart and your blood circulation, increasing the risk of conditions such as coronary heart disease, heart attack, stroke." />
+
+export const HeartD2 = () => <EffectDescription text="Both people who smoke and those who are regularly exposed to secondhand smoke are at higher risk for heart attacks." />
+
+export const HeartD3 = () => <EffectDescription text="" img={heart} />
+
+
+export const YellowD = () => <EffectDescription text="Yellowish or brownish stains on the teeth are telltale signs of long-term smoking. It also causes a bad breath." />
+
+export const YellowD2 = () => <EffectDescription text="Your sense of smell and taste can be dulled by smoking, which can decrease your appetite." />
+
+export const YellowD3 = () => <EffectDescription text="" img={teeth} />
+
+
+export const BrainD = () => <EffectDescription text="Smoking causes loss of memory, loss of brain volume. You will also lose control over emotions." />
+
+export const BrainD2 = () => <EffectDescription text="Increases risk of having a stroke by at least 50%, which can cause brain damage and death." />
+
+export const BrainD3 = () => <EffectDescription text="" img={brain}/>
