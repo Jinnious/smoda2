bf942b4e79a5d7cfbb2f38e6b0a8f71b485e5b73
Juhye Kim <jujubebe@Juhyes-iMac.local>
2020-04-23 23:48:25 -0700

    julie 7

---
+++ stories/effectinfo.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import EffectInfo from '../comps/SideEffects/effectinfo';
+
+export default {
+    title: 'Effectinfo',
+    componets: EffectInfo
+};
+
+export const MyEffectInfo = () => <EffectInfo />
