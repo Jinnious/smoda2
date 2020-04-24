255c4f7b90ebc4af0bf42ae08a32f9d2bdaf8437
= <=>
2020-04-24 05:07:24 -0700

    next step : make tutorial page

---
+++ stories/Tutorial.stories.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import AboutUs from '../comps/AboutandTutorial';
+import SubHeader from '../comps/subheader';
+import EffectDescription from '../comps/EffectDescription';
+
+
+
+
+export default {
+
+    title: "Aboutus&TutorialPage",
+   
+};
+
+
+
+export const AboutUsPage = () => <AboutUs />;
+
+export const TutorialPage = () => <AboutUs 
+
+/>;
