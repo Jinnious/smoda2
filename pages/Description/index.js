d6902822354e7b5727f54593b6ae708a625d9c31
yinayoon419 <=>
2020-04-27 01:14:14 -0700

    Side Effect description pages done except header

---
+++ pages/Description/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './description.css';
+import Button1 from '../../stories/Button.stories';
+import LungD from '../../stories/whitecomponent.stories';
+
+const defaultLung = require('./bad_lung.png');
+
+const Lungpage1 = ({img}) => <div className="about">
+
+    <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet"></link>
+
+    <div className="about_inner">
+        <div className="about_img"> <img src={img} /></div>
+        <LungD />
+        <div><Button1 /></div>
+    </div>
+</div>;
+
+Lungpage1.defaultProps = {
+    img:defaultLung,
+}
+
+
+export default Lungpage1;
