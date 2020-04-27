b0e8fc5992311bf31ac480993ed9d417dd6d5821
= <=>
2020-04-27 03:21:37 -0700

    fixing organ img size

--- comps/EffectDescription/index.js
+++ comps/EffectDescription/index.js
@@ -4,9 +4,13 @@ import BasicButton from '../buttons/button1';
 import { Button1 } from '../../stories/Button.stories';
 import Header from '../Header'
 
+
+
+
+
 const EffectDescription = ({text,img,img2}) => <div className="dsct">
+     <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet"></link>
     <Header />
-    <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet"></link>
     <div className="des_img1"> <img src={img2} /></div>
     <div className="dsct_inner">
         <div className="about_info">{text}</div>
