82b2aa0b5bb0c4c5de1807d62a1d269126cd00ba
Juhye Kim <jujubebe@Juhyes-iMac.local>
2020-04-27 11:03:17 -0700

    julie

--- comps/ingredients/about/index.js
+++ comps/ingredients/about/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './about.css';
 import BasicButton from '../../buttons/button1';
+import Headers from '../../Header';
 
 const defaultImg = require('./ammonia.png');
 
@@ -8,6 +9,8 @@ const IngredAbout = ({img,text}) => <div className="about">
 
     <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet"></link>
 
+    <div className="head"><Headers /></div>
+    <div></div>
     <div className="about_inner">
         <div className="about_img"> <img src={img} /></div>
         <div className="about_info">{text}</div>
