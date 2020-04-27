20058999e8f3e09690ff374ae6f28fdbc088fd7a
= <=>
2020-04-27 08:04:08 -0700

    ciga ing

--- comps/ingredients/ciga/index.js
+++ comps/ingredients/ciga/index.js
@@ -6,12 +6,14 @@ const defaultImg = require('../../../img/smoking.png');
 const SmokCiga = ({img}) => <div>
     <div className="ciga">
         <img src={img} />
+     
         <div className="dot_ben"></div>
         <div className="dot_but"></div>
         <div className="dot_tar"></div>
         <div className="dot_ammo"></div>
         <div className="dot_nic"></div>
-    </div>
+        </div>
+  
 </div>;
 
 SmokCiga.defaultProps = {
--- comps/ingredients/ingred/index.js
+++ comps/ingredients/ingred/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './ingred.css';
 import SubHeader from '../../subheader';
-
 import SmokCiga from '../ciga';
+import Header from '../../Header';
 
 
 const SmokIngred = () => <div className="smoking">
     <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet"></link>
-    <div><SubHeader text="Ingredients" color="#C8553D"/></div>
+
+
+
+    <Header/>
+
+    <SubHeader text="Ingredients" color="#C8553D"/>
     <div className="ciga_title">What is in a cigarette?</div>
     <div className="cigaImg"><SmokCiga /></div>
 </div>;
