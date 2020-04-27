cfb071980c1ff5bddbb81a760d87a7d64e30af13
= <=>
2020-04-27 10:53:07 -0700

    ask to henry about break down paragraph

--- stories/result.stories.js
+++ stories/result.stories.js
@@ -2,22 +2,20 @@ import React from 'react';
 // import defaultImg1 from '../comps/Quiz/Questions';
 // import defaultImg2 from '../comps/Quiz/Questions';
 import Result from '../comps/Quiz/Result';
-import QuizSubHead1  from './subhead.stories';
-import SubHeader from '../comps/subheader';
-import {GoHome} from './Button.stories';
+import {QuizSubHead } from '../stories/subhead.stories';
 
 export default {
-    title: 'Result',
-    componets: Result
+    title: 'Result'
+
 };
 
-export const Result1 = () => 
 
-<div>
-        <div><SubHeader text="Congrats!  You finished the Quiz"
-        
-        color="#F28F3C"/></div>
 
-        <GoHome/>
 
-</div>
+
+
+
+
+  
+
+export const MyTopicPage = () => <Result/>;
