771ab0b549072f855f1907121799fde66db2aec3
yinayoon419 <=>
2020-04-28 23:00:21 -0700

    navi open

--- pages/navigationopen/index.js
+++ pages/navigationopen/index.js
@@ -5,9 +5,65 @@ import './naviopen.css';
 import NavigationPage from '../navigation';
 
 const Plus = require('./plus.png');
-const NavigationOpenPage = ({img1,img2,text1,text2}) => <div id="optionpage">
+const NavigationOpenPage = ({img1,img2,text1,text2,text3,text4,text5,text6}) => <div id="optionpage">
 <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet"></link>
-<NavigationPage />
+
+<Header/>
+
+<div className="nav_bgcolor">
+<div className="basic_nav"> <div>{text1}</div>
+<div className="plus"> <img src={img1} /></div>
+</div>
+<div className="advanced_nav"> <div>{text2}</div>
+<div className="plus_1">< img src={img2} /></div>
+</div>
+<div className="navbox">
+    <div className="navtextcontainer">
+<div className="navtext" id="firsttext">{text5}</div>
+<div className="navtext" id="secondtext">{text6}</div>
+</div>
+</div>
+
+
+
+<div className="navsecondbox">
+<div className="advanced_nav"> <div>{text3}</div>
+</div>
+
+<div className="tutorial_nav"> <div>{text4}</div>
+</div>
+
+</div>
+</div>
+
+</div>
+
+
+
+
+
+
+
+NavigationOpenPage.defaultProps = {
+img1:Plus,
+img2:Plus,
+text1:"Basic",
+text2:"Advanced",
+text3:"About Us",
+text4:"Tutorial",
+text5:"Smoking",
+text6:"Drinking"
+}
+
+export default NavigationOpenPage;
+
+
+{/* <NavigationPage />
+
+
+
+
+
 <div className="navbox">
     <div className="navtextcontainer">
 <div className="navtext" id="firsttext">{text1}</div>
@@ -21,8 +77,4 @@ img1:Plus,
 img2:Plus,
 text1:"Smoking",
 text2:"Drinking"
-}
-
-
-
-export default NavigationOpenPage;
+} */}
