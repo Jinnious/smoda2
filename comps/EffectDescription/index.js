8a64446ca130e4835bdc8dcda45cf7cef852d712
yinayoon419 <=>
2020-04-29 02:37:51 -0700

    effectdescription title

--- comps/EffectDescription/index.js
+++ comps/EffectDescription/index.js
@@ -4,16 +4,17 @@ import BasicButton from '../buttons/button1';
 import { Button1 } from '../../stories/Button.stories';
 import Header from '../Header'
 import SubHeader from '../../comps/subheader';
+// import { LungFirst } from '../../stories/description.stories';
 
 
 
 
-const EffectDescription = ({text,img,img2}) => <div className="dsct">
+const EffectDescription = ({text,img,img2,title}) => <div className="dsct">
      <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet"></link>
 
     <div className="effect_head"><Header /></div>
    
-<div id="sub">    <SubHeader text="Lung" color="#588B8B"  /> </div>
+<div id="sub"><div>{title} </div></div>
     <div className="bodycontents">
     <div className="des_img1"> <img src={img2} /></div>
     <div className="dsct_inner">
@@ -30,7 +31,7 @@ const EffectDescription = ({text,img,img2}) => <div className="dsct">
 </div>;
 
 EffectDescription.defaultProps = {
-
+    title: "Lungs",
     text: "Your lungs can be very badly affected by smoking. Coughs, colds, wheezing and asthma are just the start.",
 
 }
