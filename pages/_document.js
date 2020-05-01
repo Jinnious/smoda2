9ca8626d60734981ded6b8f7e98b6bd94da404ac
= <=>
2020-05-01 12:02:13 -0700

    index.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,27 +1,27 @@
-// import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 
 
-// class MyDocument extends Document {
-//   static async getInitialProps(ctx) {
-//     const initialProps = await Document.getInitialProps(ctx)
-//     return { ...initialProps }
-//   }
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
 
-//   render() {
-//     return (
-//       <Html>
-//         <Head />
-//         <body>
-//         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,500&display=swap" rel="stylesheet">
-//     </link>
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,500&display=swap" rel="stylesheet">
+    </link>
 
-//           <Main />
-//           <NextScript />
-//         </body>
-//       </Html>
-//     )
-//   }
-// }
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
 
-// export default MyDocument
+export default MyDocument
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+import Router from 'next/router';
+import './index.css';
+
+
+const Index =() => <div id="mainapp"></div>
+
+
+export default Index;
