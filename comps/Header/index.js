b1a61ae69c0fcfe149d2d56455dba0d84cc14613
yinayoon419 <=>
2020-05-03 01:49:56 -0700

    nav new

--- comps/Header/index.js
+++ comps/Header/index.js
@@ -36,11 +36,11 @@ const Header = ({img,hamburg1,text1,text2,text3,text4,plusicon}) => <div classNa
                 <img src={plusicon}/>
             </div>
 
-            <div className="Abu">
+            <div className="nav_about">
                 {text3}
             </div>
 
-            <div className="tt">
+            <div className="nav_tut">
                 {text4}
             </div>
 
@@ -61,6 +61,7 @@ Header.defaultProps = {
     img:defaultImg,
     hamburg1:hamburg1,
     plusicon:plusicon,
+    text1: "Basic",
     text2:"Advanced",
     text3:"About Us",
     text4:"Tutorial"
