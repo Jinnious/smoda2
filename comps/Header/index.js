91662e85b922513adba953e546ec273fbc094b3c
= <=>
2020-05-15 01:38:25 -0700

    dd

--- comps/Header/index.js
+++ comps/Header/index.js
@@ -6,109 +6,26 @@ import Link from 'next/link';
 import BasicButton from '../buttons/button1';
 
 const defaultImg = require('./Logo.png');
-const hamburg1 = require('./hamburger.png');
-const plusicon = require('./plus.png');
 
 
 
-const Header = ({img,hamburg1,text1,text2,text3,text4,plusicon,text5,text6}) => <div className="header">
+
+const Header = ({img}) => <div className="header">
 
       
     <div id="circle">
     <div className="logo"><img src={img} /></div>
          
-         <div>
+         </div>
 
      
-        <div className="hambur" onClick={() =>{
-            Shownav();
-        }}><img src={hamburg1} /></div>
+        
 
-
-
-
-
-
-        <div className="navigation">
-
-            <div className="nav_basic" 
- >
-                {text1}
-
-                <div id="drsm1">
-
-                            {text5}
-                            <p></p>
-                            {text6}
-
-                            </div> 
-
-                <img src={plusicon}
-                   onClick = {() => {IsOpen();}}/>
-            </div> 
-
-         
-            
-            <div className="nav_advan"
-            >
-               {text2}
-
-                     <div id="drsm2">
-
-                    {text5}
-                    <p></p>
-                    {text6}
-                   
-                    </div> 
-
-                <img src={plusicon}
-                   onClick={()=> {Showsub();}}/>
-            </div>
-     
-
-           
-            <div className="nav_about"
-            onClick={()=> {
-                Router.push('/About')
-            }}>
-                {text3}
-            </div>
-           
-          
-
-               
-                  <div className="nav_tut"
-                   onClick={()=> {
-                    Router.push('/Tutorial')
-                }}>
-
-                {text4}
-                  </div>
-              
-
-
-        </div>
-    
-
-  
-    </div>
-    
-
-    
-    </div>
-  
- </div>
+         </div>
 
 Header.defaultProps = {
-    img:defaultImg,
-    hamburg1:hamburg1,
-    plusicon:plusicon,
-    text1:"Basic",
-    text2:"Advanced",
-    text3:"About Us",
-    text4:"Tutorial",
-    text5:"Smoking",
-    text6:"Drinking"
+    img:defaultImg
+    
 
 }
 
@@ -116,37 +33,6 @@ Header.defaultProps = {
 export default Header;
 
 
-
-// function Showsub() {
-//     document.getElementById("drsm2").classList.toggle("show");
-// }
-
-
-
-
-var menu_state = false;
-
-function Shownav() {
-
-    if(menu_state === false) {
-        MeunOpen();
-    } else {
-        MenuClose();
-    }
-    }
-
-function MeunOpen() {
-    
-    document.querySelector(".navigation").style.right = "0%";
-    menu_state = true;
-    
-}
-
-function MenuClose() {
-
-    document.querySelector(".navigation").style.right = "-130%";
-    menu_state = false;
-}
 
 
 
