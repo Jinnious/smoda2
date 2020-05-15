8ef00941b9b5295d3c3fbf97522ba5e00d33e741
Juhye Kim <jujubebe@Juhyes-iMac.local>
2020-05-15 02:52:12 -0700

    dfdfdf

--- comps/Header/index.js
+++ comps/Header/index.js
@@ -2,27 +2,152 @@ import React from 'react';
 import Router from 'next/router';
 import './header.css';
 import Link from 'next/link';
-
+import {TiThMenuOutline} from "react-icons/Ti";
+import {FaPlus} from "react-icons/Fa";
 import BasicButton from '../buttons/button1';
 
 const defaultImg = require('./Logo.png');
 
 
-
+var menu_state = false;
+var submenu_state = false;
+var submenu2_state = false;
 
 const Header = ({img}) => 
     <div className="header">
+        <div className="whitebox">
+            <div id="circle">
+                <div className="logo"><Link href="/Level"><a><img src={img} /></a></Link></div>
+            </div>
+            <TiThMenuOutline className="menu_icon" onClick={MenuToggle}/>
+            <div className="menu">
 
-      
-        <div id="circle">
-        <div className="logo"><img src={img} /></div>
-         
-         </div>
+                <section>
+                    <p onClick={SubToggle}>Basic <FaPlus className="plus_navi"/></p>
+                    <div className="submenu">
+                    <div>
+                        Smoking
+                    </div>
+                    <div>
+                        Drinking 
+                    </div>
+                    </div>
+                </section> 
 
-     
-        
 
+
+                <section>
+                    <p onClick={SubToggle2}>Advanced <FaPlus className="plus_navi"/></p>
+                    <div className="submenu2">
+                    <div>
+                        smoking
+                    </div>
+                    <div>
+                        Drinking
+                    </div>
+                    </div>
+                </section>
+
+                <section>
+                    <Link href="/About"><a><div>About Us</div></a></Link>
+                </section>
+
+                <section>
+                    <Link href="/Tutorial"><a><div>Tutorial</div></a></Link>
+                </section>       
+
+            </div>
+        </div>
     </div>
+
+function MenuOpen() {
+    document.querySelector(".menu").style.display = "block";
+    
+    setTimeout(function(){
+        document.querySelector(".menu").style.opacity = 1;
+    }, 300);
+
+    menu_state = true;
+}
+function MenuClose() {
+    setTimeout(function(){
+        document.querySelector(".menu").style.display = "none";
+    }, 300);
+
+    document.querySelector(".menu").style.opacity = "0";
+    menu_state = false;
+}
+
+function MenuToggle(){
+    console.log("menu state before if/else", menu_state);
+    if(menu_state === false){
+        MenuOpen();
+    } else {
+        MenuClose();
+    }
+    console.log("menu state after if/else", menu_state);
+}
+
+
+
+
+function SubOpen(){
+    document.querySelector(".submenu").style.display = "block";
+    setTimeout(function(){
+        document.querySelector(".submenu").style.opacity = 1;
+    }, 300);
+    submenu_state = true;
+}
+
+function SubClose() {
+    setTimeout(function(){
+        document.querySelector(".submenu").style.display = "none";
+    }, 300);
+
+    document.querySelector(".submenu").style.opacity = "0";
+    submenu_state = false;
+}
+
+function SubToggle(){
+    console.log("menu state before if/else", submenu_state);
+    if(submenu_state === false){
+        SubOpen();
+    } else {
+        SubClose();
+    }
+    console.log("menu state after if/else", submenu_state);
+}
+
+function SubOpen2(){
+    document.querySelector(".submenu2").style.display = "block";
+    setTimeout(function(){
+        document.querySelector(".submenu2").style.opacity = 1;
+    }, 300);
+    submenu2_state = true;
+}
+
+function SubClose2() {
+    setTimeout(function(){
+        document.querySelector(".submenu2").style.display = "none";
+    }, 300);
+
+    document.querySelector(".submenu2").style.opacity = "0";
+    submenu2_state = false;
+}
+
+function SubToggle2(){
+    console.log("menu state before if/else", submenu2_state);
+    if(submenu2_state === false){
+        SubOpen2();
+    } else {
+        SubClose2();
+    }
+    console.log("menu state after if/else", submenu2_state);
+}
+
+
+
+
 
 Header.defaultProps = {
     img:defaultImg
@@ -38,135 +163,3 @@ export default Header;
 
 
 
-// var open_state = false;
-
-// function IsOpen() {
-
-//     if(open_state === false) {
-//         SubOpen();
-//     } else {
-//         SubClose();
-//     }
-//     }
-
-// function SubOpen() {
-    
-//     document.querySelector(".drsm1").display = "none";
-//     open_state = true;
-    
-// }
-
-// function SubClose() {
-
-//     document.querySelector(".drsm1").display = "block";
-//     open_state = false;
-// }
-
-
-
-
-
-
-// var open_state1 = false;
-
-// function Showsub() {
-
-//     if(open_state1 === false) {
-//         SubOpen1();
-//     } else {
-//         SubClose1();
-//     }
-//     }
-
-// function SubOpen1() {
-    
-//     document.querySelector(".drsm2").display = "none";
-//     open_state1 = true;
-    
-// }
-
-// function SubClose1() {
-
-//     document.querySelector(".drsm2").display = "block";
-//     open_state1 = false;
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, {Component} from 'react';
-
-// class Card extends Component {
-
-//     constructor() 
-//     {
-//         super();
-
-//         this.state = {
-//             Showsub: false
-//         };
-
-//         this.Showsub = this.Showsub.bind(this);
-//         this.Closesub = this.Closesub.bind(this);
-
-//     }
-
-//     Showsub(event) {
-//         event.preventDefault() ;
-
-//         this.setState({Showsub: true}, () => {
-//             document.addEventListener ('click', this.Closesub);
-//         });
-//     }
-
-
-//     Closesub (event) {
-
-//         if (!this.dropdownMenu.contains(event.target)) {
-      
-//             this.setState({ Showsub: false }, () => {
-//               document.removeEventListener('click', this.closeMenu);
-//             });  
-//         }
-
-// }
-
-//     render() {
-//         return (
-//             <div>
-//             <button onClick={this.Showsub}>
-//               Show menu
-//             </button>
-
-
-//            { this.state.Showsub
-
-//            ? (
-//             <div className="menu"
-//             ref={(element) => {
-//                 this.dropdownMenu = element;
-//             }}>
-
-//             <button> Basic </button>
-//             <button> Advanced </button>
-//             <button> About us </button>
-//             <button> Tutorial </button>
-//           </div>
-//            )
-//            : (
-//                null
-//            )
-//     }
-//         </div>
-//            );
-           
-//       }
-//     }
