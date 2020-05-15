a3d0c926222ff24080eb4d19d42f5f740e5fa475
= <=>
2020-05-15 00:42:24 -0700

    ddd

--- comps/Header/index.js
+++ comps/Header/index.js
@@ -20,9 +20,9 @@ const Header = ({}) => <div className="header">
     </div>
          
          
-    <div class="dropdown">
-  <button onclick="myFunction()" class="dropbtn">Dropdown</button>
-  <div id="myDropdown" class="dropdown-content">
+    <div className="dropdown">
+  <button onclick="myFunction()" className="dropbtn">Dropdown</button>
+  <div id="myDropdown" className="dropdown-content">
     <a href="#home">Home</a>
     <a href="#about">About</a>
     <a href="#contact">Contact</a>
